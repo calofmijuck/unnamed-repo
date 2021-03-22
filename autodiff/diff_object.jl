568e2306fd5e6f783df4335bf6cca64544879773
Sungchan Yi <calofmijuck@snu.ac.kr>
2021-03-22 12:00:30 +0900

    Change field of DiffObject to two Numbers

--- autodiff/diff_object.jl
+++ autodiff/diff_object.jl
@@ -1,26 +1,35 @@
 struct DiffObject <: Number
-    f::Tuple{Float64,Float64}
+    f::Number
+    df::Number
+
+    DiffObject(f::Number, df::Number) = new(f, df)
+    DiffObject(f::Number) = new(f, one(f))
 end
 
-import Base: +, -, *, /, ^, sin, cos, tan, convert, promote_rule
+import Base: +, -, *, /, ^, sin, cos, tan, exp, log, convert, promote_rule
 
-+(x::DiffObject, y::DiffObject) = DiffObject(x.f .+ y.f)
++(x::DiffObject, y::DiffObject) = DiffObject(x.f + y.f, x.df + y.df)
 
--(x::DiffObject, y::DiffObject) = DiffObject(x.f .- y.f)
+-(x::DiffObject, y::DiffObject) = DiffObject(x.f - y.f, x.df - y.df)
 
-*(x::DiffObject, y::DiffObject) = DiffObject((x.f[1] * y.f[1], x.f[2] * y.f[1] + x.f[1] * y.f[2]))
+*(x::DiffObject, y::DiffObject) = DiffObject(x.f * y.f, x.df * y.f + x.f * y.df)
 
-/(x::DiffObject, y::DiffObject) = DiffObject((x.f[1] / y.f[1], (y.f[1] * x.f[2] - x.f[1] * y.f[2]) / y.f[1]^2))
+/(x::DiffObject, y::DiffObject) = DiffObject(x.f / y.f, (x.df * y.f - x.f * y.df) / y.f^2)
 
-^(x::DiffObject, y::DiffObject) = DiffObject((x.f[1]^y.f[1], y.f[1] * x.f[1]^(y.f[1] - 1) * x.f[2] + x.f[1]^y.f[1] * log(x.f[1]) * y.f[2]))
+^(x::DiffObject, y::DiffObject) = DiffObject(x.f^y.f, y.f * x.f^(y.f - 1) * x.df + x.f^y.f * log(x.f) * y.df)
 
-sin(x::DiffObject) = DiffObject((sin(x.f[1]), x.f[2] * cos(x.f[1])))
+sin(x::DiffObject) = DiffObject(sin(x.f), x.df * cos(x.f))
 
-cos(x::DiffObject) = DiffObject((cos(x.f[1]), -x.f[2] * sin(x.f[1])))
+cos(x::DiffObject) = DiffObject(cos(x.f), -x.df * sin(x.f))
 
-tan(x::DiffObject) = DiffObject((tan(x.f[1]), x.f[2] * sec(x.f[1])^2))
+tan(x::DiffObject) = DiffObject(tan(x.f), x.df * sec(x.f)^2)
 
-convert(::Type{DiffObject}, x::Real) = DiffObject((x, zero(x)))
+exp(x::DiffObject) = DiffObject(exp(x.f), x.df * exp(x.f))
+
+log(x::DiffObject) = DiffObject(log(x.f), x.df / x.f)
+
+convert(::Type{DiffObject}, x::Real) = DiffObject(x, zero(x))
+
 promote_rule(::Type{DiffObject}, ::Type{<:Number}) = DiffObject
 
 x = 49
