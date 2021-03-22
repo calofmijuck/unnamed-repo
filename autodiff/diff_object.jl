33cac0e1885d2f38a1c495c438a398c4019f1d75
Sungchan Yi <calofmijuck@snu.ac.kr>
2021-03-22 11:29:40 +0900

    Added implementation for trig functions

--- autodiff/diff_object.jl
+++ autodiff/diff_object.jl
@@ -2,7 +2,7 @@ struct DiffObject <: Number
     f::Tuple{Float64,Float64}
 end
 
-import Base: +, -, *, /, ^, convert, promote_rule
+import Base: +, -, *, /, ^, sin, cos, tan, convert, promote_rule
 
 +(x::DiffObject, y::DiffObject) = DiffObject(x.f .+ y.f)
 
@@ -13,6 +13,12 @@ import Base: +, -, *, /, ^, convert, promote_rule
 /(x::DiffObject, y::DiffObject) = DiffObject((x.f[1] / y.f[1], (y.f[1] * x.f[2] - x.f[1] * y.f[2]) / y.f[1]^2))
 
 ^(x::DiffObject, y::DiffObject) = DiffObject((x.f[1]^y.f[1], y.f[1] * x.f[1]^(y.f[1] - 1) * x.f[2] + x.f[1]^y.f[1] * log(x.f[1]) * y.f[2]))
+
+sin(x::DiffObject) = DiffObject((sin(x.f[1]), x.f[2] * cos(x.f[1])))
+
+cos(x::DiffObject) = DiffObject((cos(x.f[1]), -x.f[2] * sin(x.f[1])))
+
+tan(x::DiffObject) = DiffObject((tan(x.f[1]), x.f[2] * sec(x.f[1])^2))
 
 convert(::Type{DiffObject}, x::Real) = DiffObject((x, zero(x)))
 promote_rule(::Type{DiffObject}, ::Type{<:Number}) = DiffObject
